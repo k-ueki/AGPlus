b5d9da95d538a844687d494d4941df203872ab19
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2020-03-27 14:26:15 +0900

    [add]

---
+++ src/components/Class.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="class">
+      <h3>piyo!!</h3>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Class"
+    }
+</script>
+
+<style scoped>
+
+</style>
