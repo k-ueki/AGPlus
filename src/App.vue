287271472d532ca77979b906fd069e9918612d30
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2020-04-04 23:16:44 +0900

    front

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index></Index>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index.vue'
+// import ClassList from './components/ClassList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index,
+    // ClassList
   }
 }
 </script>
