3115b8a8600f9939a9b3f0d8fd5bde6e12c8f1aa
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2020-04-30 20:35:01 +0900

    [fix] scripts

--- scripts/scraping/main.go
+++ scripts/scraping/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
@@ -37,6 +38,46 @@ func gormConnect() *gorm.DB {
 	return db
 }
 
+func setQueryFactors(page string) [][2]string {
+	etarget := "ctl00%24CPH1%24rptPagerT%24ctl03%24lnkPage"
+
+	factor := make([][2]string, 30)
+	factor[0] = [2]string{"__EVENTTARGET", etarget}
+	factor[1] = [2]string{"__EVENTARGUMENT", ""}
+	factor[2] = [2]string{"__VIEWSTATEGENERATOR", "309A73F1"}
+	factor[3] = [2]string{"YR", "2019"}
+	factor[4] = [2]string{"BU", "BU1"}
+	factor[5] = [2]string{"KW", ""}
+	factor[6] = [2]string{"KM", ""}
+	factor[7] = [2]string{"KI", ""}
+	factor[8] = [2]string{"CP1", "on"}
+	// factor[8] = [2]string{"CP4", "on"}
+	factor[9] = [2]string{"YB1", "on"}
+	factor[10] = [2]string{"YB1", "on"}
+	factor[11] = [2]string{"YB2", "on"}
+	factor[12] = [2]string{"YB3", "on"}
+	factor[13] = [2]string{"YB4", "on"}
+	factor[14] = [2]string{"YB5", "on"}
+	factor[15] = [2]string{"YB6", "on"}
+	factor[16] = [2]string{"JG1", "on"}
+	factor[17] = [2]string{"JG2", "on"}
+	factor[18] = [2]string{"JG3", "on"}
+	factor[19] = [2]string{"JG4", "on"}
+	factor[20] = [2]string{"JG5", "on"}
+	factor[21] = [2]string{"JG6", "on"}
+	factor[22] = [2]string{"JG7", "on"}
+	factor[23] = [2]string{"GB1B_0", ""}
+	factor[24] = [2]string{"GKB", ""}
+	factor[25] = [2]string{"DL", "ja"}
+	factor[26] = [2]string{"ST", ""}
+	factor[27] = [2]string{"PG", page}
+	factor[28] = [2]string{"PC", "395"}
+	// factor[29] = [2]string{"PI", page_i}
+
+	return factor
+
+}
+
 func main() {
 	db := gormConnect()
 	defer db.Close()
@@ -51,46 +92,12 @@ func main() {
 		return
 	}
 
-	etarget := "ctl00%24CPH1%24rptPagerT%24ctl03%24lnkPage"
-
 	count := 0
 
 	for i := 1; i <= 395; i++ {
 		url := ""
-		page := strconv.Itoa(i)
 
-		factor := make([][2]string, 30)
-		factor[0] = [2]string{"__EVENTTARGET", etarget}
-		factor[1] = [2]string{"__EVENTARGUMENT", ""}
-		factor[2] = [2]string{"__VIEWSTATEGENERATOR", "309A73F1"}
-		factor[3] = [2]string{"YR", "2019"}
-		factor[4] = [2]string{"BU", "BU1"}
-		factor[5] = [2]string{"KW", ""}
-		factor[6] = [2]string{"KM", ""}
-		factor[7] = [2]string{"KI", ""}
-		factor[8] = [2]string{"CP1", "on"}
-		// factor[8] = [2]string{"CP4", "on"}
-		factor[9] = [2]string{"YB1", "on"}
-		factor[10] = [2]string{"YB1", "on"}
-		factor[11] = [2]string{"YB2", "on"}
-		factor[12] = [2]string{"YB3", "on"}
-		factor[13] = [2]string{"YB4", "on"}
-		factor[14] = [2]string{"YB5", "on"}
-		factor[15] = [2]string{"YB6", "on"}
-		factor[16] = [2]string{"JG1", "on"}
-		factor[17] = [2]string{"JG2", "on"}
-		factor[18] = [2]string{"JG3", "on"}
-		factor[19] = [2]string{"JG4", "on"}
-		factor[20] = [2]string{"JG5", "on"}
-		factor[21] = [2]string{"JG6", "on"}
-		factor[22] = [2]string{"JG7", "on"}
-		factor[23] = [2]string{"GB1B_0", ""}
-		factor[24] = [2]string{"GKB", ""}
-		factor[25] = [2]string{"DL", "ja"}
-		factor[26] = [2]string{"ST", ""}
-		factor[27] = [2]string{"PG", page}
-		factor[28] = [2]string{"PC", "395"}
-		// factor[29] = [2]string{"PI", page_i}
+		factor := setQueryFactors(strconv.Itoa(i))
 
 		for i, v := range factor {
 			if i == 0 {
@@ -154,30 +161,28 @@ func main() {
 
 			selection2 := doc2.Find("table.editTable > tbody > tr > td")
 
-			temp := [150]string{}
+			temp := [10]string{}
 			selection2.Each(func(index int, s *goquery.Selection) {
 				temp[index] = s.Text()
 			})
 
-			// bytes, _ := ioutil.ReadAll(res.Body)
-			// fmt.Println("nya-nn", string(bytes))
-
-			credits, _ := strconv.Atoi(temp[4])
+			credits, _ := strconv.Atoi(strings.TrimSpace(temp[4]))
 			year, _ := strconv.Atoi(temp[0])
 			class := &Class{
-				Name:        temp[1],
-				Semester:    temp[3],
+				Name:        strings.TrimSpace(temp[1]),
+				Semester:    strings.TrimSpace(temp[3]),
 				Credits:     credits,
-				Teacher:     temp[5],
-				Description: temp[7],
+				Teacher:     strings.TrimSpace(temp[5]),
+				Description: strings.TrimSpace(temp[7]),
 				Year:        year,
 				DayAt:       dayats[i],
 				Campus:      campus[i],
 			}
 			count++
 
-			fmt.Printf("%#v\n", class)
-			fmt.Println(count)
+			if count%100 == 0 {
+				fmt.Println(count)
+			}
 
 			db.Table("class").Create(class)
 		}
