00a756e2b037fa1d3fc53c7a2ca5465b17a1f486
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2020-05-17 18:09:54 +0900

    [fix] migrations

--- db/migrations/3_class.sql
+++ db/migrations/3_class.sql
@@ -2,16 +2,16 @@
 CREATE TABLE IF NOT EXISTS class (
   id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
+  opening_course_id INT(3) NOT NULL,
   semester VARCHAR(55) NOT NULL,
   credits INT(10) UNSIGNED NOT NULL,
   teacher VARCHAR(255) NOT NULL,
   description LONG,
-  course_id INT(2) NOT NULL,
   year INT(10) UNSIGNED NOT NULL,
   day_at VARCHAR(25) NOT NULL,
   campus VARCHAR(55) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (course_id) REFERENCES course(id)
+  FOREIGN KEY (course_id) REFERENCES openning_course(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- +goose Down
