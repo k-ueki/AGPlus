5b120ca4e53f4386e7be2891b7791df719131b76
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2020-05-17 15:08:43 +0900

    [fix] table&scripts

---
+++ db/migrations/9_insert_campus_into_faculty.sql
@@ -0,0 +1,4 @@
+-- +GOOSE UP
+INSERT INTO faculty (name,type,campus_id) VALUES ("青山キャンパス",1,1),("相模原キャンパス",1,2);
+
+-- +GOOSE DOWN
--- scripts/scraping/department_cource.go
+++ scripts/scraping/department_cource.go
@@ -11,15 +11,12 @@ import (
 )
 
 type (
-	Dep struct {
-		ID     int
-		Name   string
-		Campus int
-	}
-
-	Course struct {
-		DepID int
-		Name  string
+	Faculty struct {
+		Name         string
+		Type         int
+		CampusID     int
+		FacultyID    int
+		DepartmentID int
 	}
 )
 
@@ -59,15 +56,21 @@ func main() {
 	}
 
 	selection := doc.Find("div.subjectbox")
-	dep := [11]Dep{}
+	faculty := []Faculty{}
 	course := make(map[int][]string)
 
+	columnCount := 0
 	selection.Each(func(i int, s *goquery.Selection) {
 		if i == 0 {
 			selection = s.Find("ul > li")
 			selection.Each(func(j int, ss *goquery.Selection) {
-				dep[j].ID = j + 1
-				dep[j].Name = ss.Find("p.title").Text()
+				//dep[j].ID = j + 3
+				//dep[j].Name = ss.Find("p.title").Text()
+
+				//faculty[j].FacultyID = j + 3
+				//faculty[j].Name = ss.Find("p.title").Text()
+				name := ss.Find("p.title").Text()
+				columnCount = j + 3
 
 				courseURL, _ := ss.Find("a").Attr("href")
 
@@ -84,40 +87,70 @@ func main() {
 					return
 				}
 
+				campusID := 0
 				doc.Find("div.table_typeG > div.tr").Each(func(ii int, ssss *goquery.Selection) {
 					if ii == 2 {
 						campus := ssss.Find("a.link").Text()
 						if campus == "青山キャンパス" {
-							dep[j].Campus = 1
+							//dep[j].Campus = 1
+							//faculty[j].CampusID = 1
+							campusID = 1
 						} else {
-							dep[j].Campus = 2
+							//dep[j].Campus = 2
+							//faculty[j].CampusID = 2
+							campusID = 2
 						}
 					}
 				})
 
 				doc.Find("div.box > ul > li.box_item").Each(func(jj int, sssss *goquery.Selection) {
 					if sssss.Find("h5").Text() != "" {
-						course[j+1] = append(course[j+1], sssss.Find("h5").Text())
+						course[j+3] = append(course[j+3], sssss.Find("h5").Text())
 					}
 				})
+
+				f := Faculty{
+					Name:      name,
+					Type:      2,
+					CampusID:  campusID,
+					FacultyID: j + 3,
+				}
+				faculty = append(faculty, f)
+
 			})
 		}
 	})
 
-	fmt.Println(dep)
-	for _, v := range dep {
-		db.Table("department").Save(&v)
-	}
-
-	fmt.Println(course)
 	for i, v := range course {
 		for _, vv := range v {
-			course := Course{
-				DepID: i,
-				Name:  vv,
+			columnCount++
+
+			f := Faculty{}
+			if i <= 9 {
+				f = Faculty{
+					Name:         vv,
+					Type:         3,
+					CampusID:     1,
+					FacultyID:    i,
+					DepartmentID: columnCount,
+				}
+			} else {
+				f = Faculty{
+					Name:         vv,
+					Type:         3,
+					CampusID:     2,
+					FacultyID:    i,
+					DepartmentID: columnCount,
+				}
 			}
-			db.LogMode(true).Table("course").Save(course)
+			faculty = append(faculty, f)
+			//db.LogMode(true).Table("course").Save(course)
 		}
 	}
 
+	fmt.Println(faculty)
+
+	for _, v := range faculty {
+		db.LogMode(true).Table("faculty").Save(&v)
+	}
 }
